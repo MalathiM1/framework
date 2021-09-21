f960b7baeb9b98406eee54456fc1dfb426d26329
MalathiM1 <malinim1to10@gmail.com>
2021-09-21 18:31:24 -0400

    new commit

---
+++ src/main/java/com/comcast/creatorganization/ContactTest.java
@@ -0,0 +1,26 @@
+package com.comcast.creatorganization;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+
+	
+	public void createcontactTest()
+	{
+		System.out.println("contact created");
+	}
+	@Test
+	public void createorganizationTest()
+	{
+		System.out.println("organaization created");
+	}
+	@Test
+	public void deletecontactTest()
+	{
+		System.out.println("contact deleted");
+	}
+	
+
+}
